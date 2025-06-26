ee24ced2bec3b9d0c9935d2b71e010ce364f6893
uysolanki <uysolanki@gmail.com>
2025-06-26 10:58:56 +0530

    controller1

--- src/main/java/com/kiit/FirstSpringboot/controller/ProductController.java
+++ src/main/java/com/kiit/FirstSpringboot/controller/ProductController.java
@@ -1,14 +1,24 @@
 package com.kiit.FirstSpringboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kiit.FirstSpringboot.model.Product;
 
 @Controller
 public class ProductController {
 
 	@RequestMapping("/test")
-	public String test()
+	public String test(Model model)
 	{
+		Product product=new Product();
+		product.setProductCategory("Electronics");
+		product.setProductDescription("Dell Inspiron 15 Laptop");
+		product.setProductId(1);
+		product.setProductPrice(300.0);
+		product.setProductTitle("Laptop");
+		model.addAttribute("product",product);
 		return "Test";
 	}
 }
