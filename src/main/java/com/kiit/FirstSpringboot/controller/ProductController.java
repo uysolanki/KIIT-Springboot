d43fcff87a30f3210d64b7f6bf74f22b0cff3631
uysolanki <uysolanki@gmail.com>
2025-06-27 11:02:53 +0530

    request param and path variable

--- src/main/java/com/kiit/FirstSpringboot/controller/ProductController.java
+++ src/main/java/com/kiit/FirstSpringboot/controller/ProductController.java
@@ -3,8 +3,10 @@ package com.kiit.FirstSpringboot.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kiit.FirstSpringboot.model.Product;
@@ -42,9 +44,55 @@ public class ProductController {
 		return "Product Addedd Successfully";
 	}
 	
+	@RequestMapping("/addProduct1")
+	public String addProduct1()
+	{
+		Product product=Product.builder()
+				.productCategory("Jewelry")
+				.productPrice(100.0)
+				.productTitle("Gold Ring")
+				.build();
+		productService.addProduct(product);
+		return "Product Addedd Successfully";
+	}
+	
 	@GetMapping("/getProduct")
 	public Product getProduct()
 	{
 		return productService.getProduct(1);
 	}
+	
+	@PostMapping("/addProductByRequestParam")
+	public String addProductByRequestParam(@RequestParam("ptitle") String title,
+										   @RequestParam("pdesc") String description,
+										   @RequestParam("pcategory") String category,
+										   @RequestParam("prpice") double price)
+	{
+		Product product=Product.builder()
+				.productCategory(category)
+				.productPrice(price)
+				.productTitle(title)
+				.productDescription(description)
+				.build();
+		productService.addProduct(product);
+		return "Product Addedd Successfully";
+	}
+	
+	
+	@PostMapping("/addProductByPathVariable/{a}/{b}/{c}/{d}")
+	public String addProductByPathVariable(@PathVariable("a") String title,
+										   @PathVariable("b") String description,
+										   @PathVariable("c") String category,
+										   @PathVariable("d") double price)
+	{
+		Product product=Product.builder()
+				.productCategory(category)
+				.productPrice(price)
+				.productTitle(title)
+				.productDescription(description)
+				.build();
+		productService.addProduct(product);
+		return "Product Addedd Successfully";
+	}
+	
 }
--- src/main/java/com/kiit/FirstSpringboot/model/Product.java
+++ src/main/java/com/kiit/FirstSpringboot/model/Product.java
@@ -4,22 +4,42 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
 public class Product {
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int productId;
-	private String productTitle;
-	private String productDescription;
-	private String productCategory;
-	private double productPrice;
+	int productId;
+	
+	@NotNull(message = "Product Title cannot be null") 
+	@Size(min = 3, message = "First name must contain at least 3 characters") 
+	String productTitle;
+	
+	@NotNull(message = "Product Description cannot be null") 
+	String productDescription;
+	
+	@NotNull(message = "Product Title cannot be null")
+	String productCategory;
+	@NotNull(message = "Product Price cannot be null") 
+	@Min(value = 100, message = "Product Price must be at least 100") 
+	@Max(value = 5000, message = "Product Price must be less than or equal to 5000") 
+	@Positive
+	double productPrice;
 }
